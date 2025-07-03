48b40256e44320e1ac463851e65ccc6547397c69
Andres Avendano <anavguitar@gmail.com>
2025-07-02 18:36:22 -0700

    Added Module 2 and print() exercise

---
+++ Module_2/the_print_function.py
@@ -0,0 +1,8 @@
+# Expected output  Programming***Essentials***in...Python
+
+print("Programming", "Essentials", "in")
+print("Python")
+
+
+print("Programming", "Essentials", "in", sep="***", end="...")
+print("Python")
