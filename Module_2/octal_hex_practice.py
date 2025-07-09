cd3ed98e0828033b39d2b88cfffcaad3c5f72e4d
Andres Avendano <anavguitar@gmail.com>
2025-07-08 18:31:28 -0700

    Completed print_function exercises and started octal_hex_practice exercises

---
+++ Module_2/octal_hex_practice.py
@@ -0,0 +1,29 @@
+"""
+Exercises: Decimal to Octal/Hexadecimal
+Translate the following decimal numbers to their octal and hexadecimal equivalents:
+
+Decimal 12
+
+Decimal 27
+
+Decimal 100
+
+Decimal 255
+
+Decimal 400
+
+"""
+
+# Decimal 12
+twelve_oct = 0o14
+twelve_hex = 0xb
+print(twelve_oct, twelve_hex)
+# Decimal 27
+twentySeven_oct = 0o33
+twentySeven_hex = 0x1b
+print(twentySeven_oct, twentySeven_hex)
+# Decimal 100
+hundred_oct = 0o144
+hundred_hex = 0x64
+print(hundred_oct, hundred_hex)
+
--- Module_2/the_print_function.py
+++ Module_2/the_print_function.py
@@ -1,7 +1,7 @@
 # Expected output  Programming***Essentials***in...Python
 
-print("Programming", "Essentials", "in")
-print("Python")
+# print("Programming", "Essentials", "in")
+# print("Python")
 
 
 print("Programming", "Essentials", "in", sep="***", end="...")
