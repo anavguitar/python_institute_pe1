1f12069956e289ea1b06c13c8e5b8ae8f4d3dc84
Andres Avendano <anavguitar@gmail.com>
2025-07-19 02:52:33 -0700

    completed the exercises and added some notes for future me

--- Module_2/operators_paranthesis.py
+++ Module_2/operators_paranthesis.py
@@ -7,3 +7,15 @@ print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
 
 # At Step 2, order of operations kicks in from LEFT to RIGHT
 # Thus, 5 * 112
+
+print((2 ** 4), (2 * 4.), (2 * 4))
+# ((16), (8.0), (8))
+
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+# ((-.5), (.5), (0), (-1))
+# -2 // 4 rounds DOWN to -1
+
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))
+# ((-2), (2), (512))
+# Ex 2: 4 goes into 2 ZERO times, thus the remainder is 2.
+# Ex 1: Since the divisor is -4, the output is -2
