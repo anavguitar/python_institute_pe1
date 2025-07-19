02b48c89740a54b06e3f257654ed15ab71fa798f
Andres Avendano <anavguitar@gmail.com>
2025-07-18 20:16:36 -0700

    completed '2.2.1.11 LAB: Python literals - strings'

---
+++ Module_2/python_strings.py
@@ -0,0 +1,8 @@
+'''
+Write a one-line piece of code, using the print() function, as well as the newline and escape characters, to match the expected result outputted on three lines.
+"I'm"
+""learning""
+"""Python"""
+'''
+
+print('"I\'m"\n""learning""\n"""Python"""')
